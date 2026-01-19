2ae4e181945b91937dfb08620d05a7de7f4e34be
Bumblebash <ssimbwabashir777@gmail.com>
2026-01-19 15:20:00 +0200

    Modified datatypes and changed Column names for malaria data

--- Malaria data/DataQuality Checks2.sql
+++ Malaria data/DataQuality Checks2.sql
@@ -1,15 +1,15 @@
 USE PRACTICE1;
 
-SELECT * from malaria_data;
+SELECT * from malaria_data1;
 
 SELECT COUNT(DISTINCT(column1)) FROM malaria_data;
 
 SELECT DISTINCT(column1) FROM malaria_data;
 
-
+--Counting the total Number of Columns Present
 SELECT COUNT(*) AS NumberOfColumns
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'malaria_data'
+WHERE TABLE_NAME = 'malaria_data1'
 
 
 
@@ -18,162 +18,166 @@ WHERE TABLE_NAME = 'malaria_data'
 EXEC sp_rename 'malaria_data.[column1]',  'Region', 'COLUMN';
 EXEC sp_rename 'malaria_data.[column2]',  'District', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column3]',  '0-28Days_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column4]',  '0_28Days_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column5]',  '0_28Days_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[0-28_DAYS_Female]',  '0_28Days_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_0_28_DAYS_MALE]',  '0_28Days_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_0_28_DAYS_TOTAL]',  '0_28Days_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column6]',  '10_19Days_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column7]',  '10_19Days_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column8]',  '10_19Days_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_10_19_YEARS_FEMALE]',  '10_19Days_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_10_19_YEARS_MALE]',  '10_19Days_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_10_19YRS_TOTAL]',  '10_19Days_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column9]',  '20+YRS_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column10]', '20+YRS_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column11]', '20+YRS_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_20_YRS_FEMALE]',  '20+YRS_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_20_YRS_MALE]', '20+YRS_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_20_YRS_TOTAL]', '20+YRS_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column12]', '5_9YRS_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column13]', '5_9YRS_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column14]', '5_9YRS_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_5_9YRS_FEMALE]', '5_9YRS_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_5_9YRS_MALE]', '5_9YRS_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_5_9YRS_TOTAL]', '5_9YRS_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column15]', '29Days_4Yrs_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column16]', '29Days_4Yrs_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column17]', '29Days_4Yrs_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_29_DAYS_4YRS_FEMALE]', '29Days_4Yrs_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_29DAYS_4YRS_MALE]', '29Days_4Yrs_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_29_DAYS_4YRS_TOTAL]', '29Days_4Yrs_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column18]', '0_28Days(BSRDT)_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column19]', '0_28Days(BSRDT)_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column20]', '0_28Days(BSRDT)_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_]', '0_28Days(BSRDT)_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_0_28_DAYS_B_S_RDT_MALE]', '0_28Days(BSRDT)_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_0_28_DAYS_B_S_RDT_TOTAL]', '0_28Days(BSRDT)_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column21]', '10_19YRS(BSRDT)_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column22]', '10_19YRS(BSRDT)_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column23]', '10_19YRS(BSRDT)_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_10_19_YRS_B_S_RDT_FEMALE]', '10_19YRS(BSRDT)_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_10_19_YRS_B_S_RDT_MALE]', '10_19YRS(BSRDT)_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_10_19_YRS_B_S_RDT_TOTAL]', '10_19YRS(BSRDT)_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column24]', '20+YRS_(BSRDT)_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column25]', '20+YRS_(BSRDT)_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column26]', '20+YRS_(BSRDT)_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_20_B_S_RDT_FEMALE]', '20+YRS_(BSRDT)_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_20_B_S_RDT_MALE]', '20+YRS_(BSRDT)_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_20_B_S_RDT_TOTAL]', '20+YRS_(BSRDT)_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column27]', '29Days_4Yrs(BSRDT)_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column28]', '29Days_4Yrs(BSRDT)_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column29]', '29Days_4Yrs(BSRDT)_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_29DAYS_4YRS_B_S_RDT_FEMALE]', '29Days_4Yrs(BSRDT)_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_29DAYS_4YRS_B_S_RDT_MALE]', '29Days_4Yrs(BSRDT)_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_29DAYS_4_YRS_B_S_RDT_TOTAL]', '29Days_4Yrs(BSRDT)_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column30]', '5_9YRS(BSRDT)_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column31]', '5_9YRS(BSRDT)_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column32]', '5_9YRS(BSRDT)_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_5_9YRS_B_S_RDT_FEMALE]', '5_9YRS(BSRDT)_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_5_9YRS_B_S_RDT_MALE]', '5_9YRS(BSRDT)_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_5_9YRS_B_S_RDT_TOTAL]', '5_9YRS(BSRDT)_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column33]', 'TotalCasesConfirmed_BSRDT', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[TOTAL_CASES_CONFIRMED_B_S_RDT]', 'TotalCasesConfirmed_BSRDT', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column34]', '0_28Days_CT_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column35]', '0_28Days_CT_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column36]', '0_28Days_CT_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_0_28_DAYS_FEMALE_CT]', '0_28Days_CT_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_0_28_DAYS_MALE_CT]', '0_28Days_CT_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_0_28_DAYS_TOTAL_CT]', '0_28Days_CT_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column37]', '10_19YRS_CT_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column38]', '10_19YRS_CT_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column39]', '10_19YRS_CT_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_10_19_YRS_FEMALE_CT]', '10_19YRS_CT_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_10_19_YRS_MALE_CT]', '10_19YRS_CT_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_10_19_YRS_CT_TOTAL]', '10_19YRS_CT_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column40]', '20+YRS_CT_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column41]', '20+YRS_CT_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column42]', '20+YRS_CT_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_20_YRS_FEMALE_CT]', '20+YRS_CT_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_20_YRS_MALE_CT]', '20+YRS_CT_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_20_YRS_CT_TOTAL]', '20+YRS_CT_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column43]', '29Days_4Yrs_CT_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column44]', '29Days_4Yrs_CT_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column45]', '29Days_4Yrs_CT_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_29DAYS_4YRS_FEMALE_CT]', '29Days_4Yrs_CT_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_29_DAYS_4YRS_MALE_CT]', '29Days_4Yrs_CT_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_29_DAYS_4YRS_CT_TOTAL]', '29Days_4Yrs_CT_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column46]', '5_9YRS_CT_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column47]', '5_9YRS_CT_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column48]', '5_9YRS_CT_Total', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column49]', 'Preg_10_19', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column50]', 'Preg_20Plus', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column51]', 'Preg_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_5_9YRS_FEMALE_CT]', '5_9YRS_CT_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_5_9YRS_MALE_CT]', '5_9YRS_CT_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_5_9YRS_CT_TOTAL]', '5_9YRS_CT_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[PREG_10_19YRS]', 'Preg_10_19', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[PREG_20_YRS]', 'Preg_20Plus', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[TOTAL_PREG]', 'Preg_Total', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column52]', 'TotalCases_Female', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column53]', 'TotalCases_Male', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column54]', 'GrandTotalCases', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[TOTAL_CASES_FEMALE]', 'TotalCases_Female', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[TOTAL_CASES_MALE]', 'TotalCases_Male', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[GRAND_TOTAL_CASES]', 'GrandTotalCases', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column55]', 'Under5_Female_Total', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column56]', 'Under5_Male_Total', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column57]', 'Under5_GrandTotal', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_5YRS_FEMALES_TOTAL]', 'Under5_Female_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_5YRS_MALE_TOTAL]', 'Under5_Male_Total', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[_5YRS_GRAND_TOTAL]', 'Under5_GrandTotal', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column58]', 'Area_km2', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column59]', 'TotalPopulation', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column60]', 'PopulationDensityPerKm2', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[AREA_Km2]', 'Area_km2', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[TOTAL_POPULATION]', 'TotalPopulation', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[POPULATION_DENSITY_People_per_km2]', 'PopulationDensityPerKm2', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column61]', 'Latitude', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column62]', 'Longitude', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[CORDINATES_LATITUDE_N]', 'Latitude', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[CORDINATES_LONGITUDE_E]', 'Longitude', 'COLUMN';
 
-EXEC sp_rename 'malaria_data.[column63]', 'Year', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column64]', 'IncidenceRate', 'COLUMN';
-EXEC sp_rename 'malaria_data.[column65]', 'PopulationDensity', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[Year]', 'Year', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[Incidence_Rate]', 'IncidenceRate', 'COLUMN';
+EXEC sp_rename 'malaria_data1.[POPULATION_DENSITY]', 'PopulationDensity', 'COLUMN';
 
 
 --Number of Rows 
 SELECT COUNT(*)  From malaria_data;
 
+
+--19/01/2026
 --Changing Datatypes
 
-  ALTER TABLE malaria_data ALTER  COLUMN Region NVARCHAR(30);
-  ALTER TABLE malaria_data  ALTER  COLUMN District NVARCHAR(30);
-  ALTER TABLE malaria_data  ALTER COLUMN "0-28Days_Female" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "0_28Days_Male" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "0_28Days_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "10_19Days_Female" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "10_19Days_Male" INT;
-  ALTER TABLE malaria_data ALTER  COLUMN "10_19Days_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "20+YRS_Female" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "20+YRS_Male" INT;
-  ALTER TABLE malaria_data ALTER  COLUMN "20+YRS_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "5_9YRS_Female" INT;
-  ALTER TABLE malaria_data ALTER  COLUMN "5_9YRS_Male" INT;
-  ALTER TABLE malaria_data ALTER  COLUMN "5_9YRS_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "29Days_4Yrs_Female" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "29Days_4Yrs_Male" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "29Days_4Yrs_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "0_28Days(BSRDT)_Female" INT;
-  ALTER  TABLE malaria_data ALTER  COLUMN "0_28Days(BSRDT)_Male" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "0_28Days(BSRDT)_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "10_19YRS(BSRDT)_Female" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "10_19YRS(BSRDT)_Male" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "10_19YRS(BSRDT)_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "20+YRS_(BSRDT)_Female" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "20+YRS_(BSRDT)_Male" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "20+YRS_(BSRDT)_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "29Days_4Yrs(BSRDT)_Female" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "29Days_4Yrs(BSRDT)_Male" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "29Days_4Yrs(BSRDT)_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "5_9YRS(BSRDT)_Female" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "5_9YRS(BSRDT)_Male" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "5_9YRS(BSRDT)_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "TotalCasesConfirmed_BSRDT" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "0_28Days_CT_Female" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "0_28Days_CT_Male" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "0_28Days_CT_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "10_19YRS_CT_Female" INT;
-  ALTER  TABLE malaria_data ALTER COLUMN "10_19YRS_CT_Male" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "10_19YRS_CT_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "20+YRS_CT_Female" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "20+YRS_CT_Male" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "20+YRS_CT_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "29Days_4Yrs_CT_Female" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "29Days_4Yrs_CT_Male" INT;
-  ALTER  TABLE malaria_data ALTER COLUMN "29Days_4Yrs_CT_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "5_9YRS_CT_Female" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "5_9YRS_CT_Male" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "5_9YRS_CT_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "Preg_10_19" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "Preg_20Plus" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "Preg_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "TotalCases_Female" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "TotalCases_Male" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "GrandTotalCases" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "Under5_Female_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "Under5_Male_Total" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "Under5_GrandTotal" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "Area_km2" DECIMAL(18,8);
-  ALTER TABLE malaria_data ALTER COLUMN TotalPopulation DECIMAL(18,8);
-  ALTER TABLE malaria_data ALTER COLUMN "PopulationDensityPerKm2" DECIMAL(18,8);
-  ALTER TABLE malaria_data ALTER COLUMN "Latitude" DECIMAL(9,6);
-  ALTER TABLE malaria_data ALTER COLUMN "Longitude" DECIMAL(9,6);
-  ALTER TABLE malaria_data ALTER COLUMN "Year" INT;
-  ALTER TABLE malaria_data ALTER COLUMN "IncidenceRate" DECIMAL(18,8);
-  ALTER TABLE malaria_data ALTER COLUMN "PopulationDensity" DECIMAL(18,8);
+  ALTER TABLE malaria_data1 ALTER  COLUMN Region NVARCHAR(30);
+  ALTER TABLE malaria_data1  ALTER  COLUMN District NVARCHAR(30);
+  ALTER TABLE malaria_data1  ALTER COLUMN "0-28Days_Female" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "0_28Days_Male" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "0_28Days_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "10_19Days_Female" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "10_19Days_Male" INT;
+  ALTER TABLE malaria_data1 ALTER  COLUMN "10_19Days_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "20+YRS_Female" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "20+YRS_Male" INT;
+  ALTER TABLE malaria_data1 ALTER  COLUMN "20+YRS_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "5_9YRS_Female" INT;
+  ALTER TABLE malaria_data1 ALTER  COLUMN "5_9YRS_Male" INT;
+  ALTER TABLE malaria_data1 ALTER  COLUMN "5_9YRS_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "29Days_4Yrs_Female" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "29Days_4Yrs_Male" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "29Days_4Yrs_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "0_28Days(BSRDT)_Female" INT;
+  ALTER  TABLE malaria_data1 ALTER  COLUMN "0_28Days(BSRDT)_Male" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "0_28Days(BSRDT)_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "10_19YRS(BSRDT)_Female" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "10_19YRS(BSRDT)_Male" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "10_19YRS(BSRDT)_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "20+YRS_(BSRDT)_Female" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "20+YRS_(BSRDT)_Male" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "20+YRS_(BSRDT)_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "29Days_4Yrs(BSRDT)_Female" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "29Days_4Yrs(BSRDT)_Male" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "29Days_4Yrs(BSRDT)_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "5_9YRS(BSRDT)_Female" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "5_9YRS(BSRDT)_Male" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "5_9YRS(BSRDT)_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "TotalCasesConfirmed_BSRDT" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "0_28Days_CT_Female" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "0_28Days_CT_Male" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "0_28Days_CT_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "10_19YRS_CT_Female" INT;
+  ALTER  TABLE malaria_data1 ALTER COLUMN "10_19YRS_CT_Male" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "10_19YRS_CT_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "20+YRS_CT_Female" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "20+YRS_CT_Male" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "20+YRS_CT_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "29Days_4Yrs_CT_Female" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "29Days_4Yrs_CT_Male" INT;
+  ALTER  TABLE malaria_data1 ALTER COLUMN "29Days_4Yrs_CT_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "5_9YRS_CT_Female" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "5_9YRS_CT_Male" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "5_9YRS_CT_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "Preg_10_19" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "Preg_20Plus" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "Preg_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "TotalCases_Female" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "TotalCases_Male" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "GrandTotalCases" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "Under5_Female_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "Under5_Male_Total" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "Under5_GrandTotal" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "Area_km2" DECIMAL(18,8);
+  ALTER TABLE malaria_data1 ALTER COLUMN TotalPopulation DECIMAL(18,8);
+  ALTER TABLE malaria_data1 ALTER COLUMN "PopulationDensityPerKm2" DECIMAL(18,8);
+  ALTER TABLE malaria_data1 ALTER COLUMN "Latitude" DECIMAL(9,6);
+  ALTER TABLE malaria_data1 ALTER COLUMN "Longitude" DECIMAL(9,6);
+  ALTER TABLE malaria_data1 ALTER COLUMN "Year" INT;
+  ALTER TABLE malaria_data1 ALTER COLUMN "IncidenceRate" DECIMAL(18,8);
+  ALTER TABLE malaria_data1 ALTER COLUMN "PopulationDensity" DECIMAL(18,8);
 
+  --Checking out New datatypes 
+  EXEC sp_help malaria_data1;
 
   --Realise Duplicates in Region Column and District Column
   SELECT DISTINCT(Region) 
