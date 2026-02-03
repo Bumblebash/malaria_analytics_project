548e47dd47e2a037f3100f3893aecba7bf01d892
Bumblebash <ssimbwabashir777@gmail.com>
2026-02-03 06:37:17 +0200

    All the latest changes

--- Malaria data/DataWareHouseMalaria.sql
+++ Malaria data/DataWareHouseMalaria.sql
@@ -1,6 +1,7 @@
 USE PRACTICE1;
---DATE DIMENSION (DateDim)
 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--DATE DIMENSION (DateDim)
 CREATE TABLE DimDate(
 	DateKey INT PRIMARY KEY,
 	FullDate DATE NOT NULL,
@@ -12,5 +13,113 @@ CREATE TABLE DimDate(
 
 );
 
---Confirming 
+--Confirming datatypes for Date Dimensional table.
 Exec sp_help DimDate;
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--REGION DIMENSIONAL TABLE(DimRegion)
+CREATE TABLE DimRegion (
+   RegionKey INT IDENTITY(1,1) PRIMARY KEY,
+   Region VARCHAR(100) NOT NULL UNIQUE
+);
+--NOTE : Unique Prevents duplicates like "Central" Vs "central"
+
+--Confirming datatypes for Region Dimensional Table
+Exec sp_help DimRegion;
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+---DISTRICT DIMESNSION(DimDistrict)
+
+CREATE TABLE DimDistrict(
+    DistrictKey INT IDENTITY(1,1) PRIMARY KEY,
+	DistrictName VARCHAR(120) NOT NULL,
+	RegionKey   INT NOT NULL,
+	IsCity BIT DEFAULT 0,
+
+
+	CONSTRAINT FK_District_Region
+		FOREIGN KEY (RegionKey)
+		REFERENCES DimRegion(RegionKey)
+);
+Exec sp_help DimDistrict;
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--AGE GROUP DIMENSION(DimAgeGroup)
+CREATE TABLE DimAgeGroup(
+       AgeKey INT IDENTITY(1,1) PRIMARY KEY,
+	   AgeGroup VARCHAR(50) NOT NULL UNIQUE,
+	   MinAge INT,
+	   MaxAge INT
+);
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--GENDER DIMESNSION(DimGender)
+CREATE TABLE DimGender(
+    GenderKey INT IDENTITY(1,1) PRIMARY KEY,
+	Gender VARCHAR(10) NOT NULL UNIQUE 
+);
+
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+--MAIN FACT TABLE (FactMalria)
+CREATE  TABLE FactMalaria (
+  FACTID BIGINT IDENTITY(1,1) PRIMARY KEY,
+
+  DateKey  INT NOT NULL,
+  DistrictKey INT NOT NULL,
+  RegionKey INT NOT NULL,
+  AgeKey INT NOT NULL,
+  GenderKey   INT NOT NULL,
+
+
+  ConfirmedCases INT NOT NULL,
+  TreatedCases INT NOT NULL,
+  Population INT NOT NULL,
+
+
+  LoadDate  DATETIME DEFAULT GETDATE(),
+
+
+  CONSTRAINT FK_Fact_Date
+		FOREIGN KEY (DateKey)
+		REFERENCES DimDistrict(DistrictKey),
+
+
+  CONSTRAINT FK_Fact_District
+		FOREIGN KEY (DistrictKey)
+		REFERENCES DimDistrict(DistrictKey),
+
+
+  CONSTRAINT Fk_Fact_region
+		FOREIGN KEY(RegionKey)
+		REFERENCES DimRegion(RegionKey),
+
+  CONSTRAINT Fk_Fact_Age
+		FOREIGN KEY (AgeKey)
+		REFERENCES DimAgeGroup(AgeKey),
+
+
+  CONSTRAINT Fk_Fact_Gender
+			FOREIGN KEY (GenderKey)
+			REFERENCES DimGender(GenderKey)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
