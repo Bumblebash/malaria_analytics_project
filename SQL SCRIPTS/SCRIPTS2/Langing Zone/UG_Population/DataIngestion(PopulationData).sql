08a4fa6fe222fa88585568ad4bb6eea088a73f3b
Bumblebash <ssimbwabashir777@gmail.com>
2026-04-20 14:41:19 +0200

    Script for ingesting population data is still under cotsruction

---
+++ SQL SCRIPTS/SCRIPTS2/Langing Zone/UG_Population/DataIngestion(PopulationData).sql
@@ -0,0 +1,20 @@
+USE MalariaLanding_DB;
+
+INSERT INTO Stg_Malaria_Permanent(
+     Population_2020,
+     Population_2021,
+     Popualation_2022,
+     Popaulation_2023,
+     Population_2024
+)
+SELECT(
+)
+
+WITH Growth_Rate AS (
+    SELECT 
+         District,
+         Region,
+
+
+
+)
